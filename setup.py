194b140d66701a1aae63db6648a29a5e6c70f284
andrieu <pierre.andrieu@lri.fr>
2020-09-24 23:50:13 +0200

    update setup file, remove c extention

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from distutils.extension import Extension
 
 
 def readme():
@@ -8,7 +7,7 @@ def readme():
 
 
 setup(name='corankco',
-      version='0.0.5',
+      version='0.0.6',
       description='Kemeny-Young method for rank aggregation of incomplete rankings with ties',
       long_description=readme(),
       url='https://github.com/pierreandrieu/corankco',
@@ -16,12 +15,11 @@ setup(name='corankco',
       author_email='pierre.andrieu@lilo.org',
       license='MIT',
       packages=find_packages(include=['corankco', 'corankco.*']),
-      ext_modules=[Extension("bioconsertcimpl", ["bioconsertcimpl.c"])],
       python_requires='>=3',
       zip_safe=False,
       install_requires=['numpy',
                         'python-igraph',
                         'pulp==2.3',
-                        'bioconsertinc==0.0.2',
+                        'bioconsertinc==0.0.3',
                         ]
       )

3a6dd3e57e8441aa9e006ffa0628323db3ae5023
andrieu <pierre.andrieu@lri.fr>
2021-11-27 10:24:44 +0100

    update version of package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='corankco',
-      version='2.1.0',
+      version='2.1.2',
       description='Kemeny-Young method for rank aggregation of incomplete rankings with ties',
       long_description=readme(),
       url='https://github.com/pierreandrieu/corankco',

51541511f029957e8aaa46c2f81b573904059dd7
andrieu <pierre.andrieu@lri.fr>
2023-06-28 18:23:34 +0200

    Improve class: * remove T type * int or str replaced by Element * add types to some arguments * get List of Set instead of List of List for a ranking

--- corankco/utils.py
+++ corankco/utils.py
@@ -1,105 +1,104 @@
-from typing import Callable, List, TypeVar, Tuple
+from typing import Callable, List, Tuple, Set
 from urllib.request import urlopen
+from corankco.element import Element
 import os
-T = TypeVar('T')
 
 
-def parse_ranking_with_ties(ranking: str, converter: Callable[[str], T]) -> List[List[T]]:
+def parse_ranking_with_ties(ranking: str, converter: Callable[[str], Element]) -> List[Set[Element]]:
+    """
+    Function to parse rankings with ties.
+    The rankings can be rankings of int or str
+    Example: [{Bob}, {Martin, John}] or [{0}, {2, 1}]
+    """
     ranking = ranking.strip().split(":")[-1].strip()
 
-    # to manage the "syn" datasets of java rank-n-ties
-    # if ranking[-1] == ":":
-    #    ranking = ranking[:-1]
-    if len(ranking[ranking.find('[')+1:ranking.rfind(']')].strip()) == 0 or ranking.endswith("[[]]"):
+    if len(ranking[ranking.find('[') + 1:ranking.rfind(']')].strip()) == 0 or ranking.endswith("[[]]"):
         return []
+
     ret = []
     st = ranking.find('[', ranking.find('[') + 1)
     en = ranking.find(']')
     ranking_end = ranking.rfind(']')
     old_en = en
+
     if ranking[ranking_end + 1:] != "":
-        raise ValueError("remaining chars at the end: '%s'" % ranking[ranking_end + 1:])
+        raise ValueError(f"remaining chars at the end: '{ranking[ranking_end + 1:]}'")
+
     while st != -1 and en != -1:
-        bucket = []
+        bucket = set()
         for s in ranking[st + 1:  en].split(","):
             elt_str = s.strip()
             if elt_str == "":
-                print("ranking with problem : " + ranking)
-                raise ValueError("Empty element in `%s` between chars %i and %i" % (ranking[st + 1:  en], st + 1, en))
-            bucket.append(converter(elt_str))
+                print(f"ranking with problem : {ranking}")
+                raise ValueError(f"Empty element in `{ranking[st + 1:  en]}` between chars {st + 1} and {en}")
+            bucket.add(converter(elt_str))
         ret.append(bucket)
         st = ranking.find('[', en + 1, ranking_end)
         old_en = en
         en = ranking.find(']', max(en + 1, st + 1), ranking_end)
+
     if st != en:
         if st == -1:
             raise ValueError("missing open bucket")
         if en == -1:
             raise ValueError("missing closing bucket")
+
     if ranking[old_en + 1:ranking_end] != "":
-        raise ValueError("misplaced chars: '%s'" % ranking[old_en + 1:ranking_end])
+        raise ValueError(f"misplaced chars: '{ranking[old_en + 1:ranking_end]}'")
     return ret
 
 
-def parse_ranking_with_ties_of_str(ranking: str) -> List[List[str]]:
-    return parse_ranking_with_ties(
-        ranking=ranking,
-        converter=lambda x: x
-    )
+def parse_ranking_with_ties_of_str(ranking: str) -> List[Set[Element]]:
+    return parse_ranking_with_ties(ranking, lambda x: Element(x))
 
 
-def parse_ranking_with_ties_of_int(ranking: str) -> List[List[int]]:
-    return parse_ranking_with_ties(
-        ranking=ranking,
-        converter=lambda x: int(x)
-    )
+def parse_ranking_with_ties_of_int(ranking: str) -> List[Set[Element]]:
+    return parse_ranking_with_ties(ranking, lambda x: Element(int(x)))
 
 
-def get_rankings_from_file(file: str) -> List[List[List[int or str]]]:
-    rankings = []
-    file_rankings = open(file, "r")
-
-    # to manage the "step" datasets of java rank-n-ties
+def get_rankings_from_file(file: str) -> List[List[Set[Element]]]:
     ignore_lines = ["%"]
-    lines = file_rankings.read().replace("\\\n", "")
-    for line in lines.split("\n"):
-        if len(line) > 2 and line[0] not in ignore_lines:
-            rankings.append(parse_ranking_with_ties_of_str(line))
-    file_rankings.close()
-    return rankings
+
+    with open(file, "r") as file_rankings:
+        lines = file_rankings.read().replace("\\\n", "")
+
+    return [parse_ranking_with_ties_of_str(line)
+            for line in lines.split("\n") if len(line) > 2 and line[0] not in ignore_lines]
 
 
-def get_rankings_from_folder(folder: str) -> List[Tuple[List[List[List[int or str]]], str]]:
+def get_rankings_from_folder(folder: str) -> List[Tuple[List[List[Set[Element]]], str]]:
     res = []
     if not folder.endswith(os.path.sep):
         folder += os.path.sep
+
     for file_path in sorted(os.listdir(folder)):
-        rankings = get_rankings_from_file(folder+file_path)
+        rankings = get_rankings_from_file(folder + file_path)
         res.append((rankings, file_path))
+
     return res
 
 
-def dump_ranking_with_ties_to_str(ranking: List[List[int or str]]) -> str:
+def dump_ranking_with_ties_to_str(ranking: List[Set[Element]]) -> str:
     if len(ranking) == 0:
         return "[]"
     else:
         return '[' + ','.join(['[' + ','.join([str(e) for e in b]) + ']' for b in ranking]) + ']'
 
 
-def import_rankings_from_url(url_path: str) -> List[List[List[int or str]]]:
-    data = urlopen(url_path)  # read only 20 000 chars
-
+def import_rankings_from_url(url_path: str) -> List[List[Set[Element]]]:
+    data = urlopen(url_path)
     rankings = []
-    # to manage the "step" datasets of java rank-n-ties
     ignore_lines = ["%"]
+
     for line_file in data:
         line_str = line_file.decode('utf-8')
         if len(line_str) > 2 and line_str[0] not in ignore_lines:
             rankings.append(parse_ranking_with_ties_of_int(line_str))
+
     return rankings
 
 
-def write_rankings(rankings: List[List[List[int or str]]], path: str):
+def write_rankings(rankings: List[List[Set[Element]]], path: str):
     if not os.path.isdir(path) and not os.path.isfile(path):
         if os.path.isdir(os.path.abspath(os.path.join(path, os.pardir))):
             file = open(path, "w")
@@ -119,17 +118,19 @@ def get_parent_path(path: str) -> str:
     return os.path.abspath(os.path.join(path, os.pardir))
 
 
-def join_paths(path, *paths) -> str:
+def join_paths(path: str, *paths) -> str:
     return os.path.join(path, *paths)
 
 
-def can_be_created(path_to_check) -> bool:
+def can_be_created(path_to_check: str) -> bool:
     return not os.path.isdir(path_to_check) and not os.path.isfile(path_to_check)
 
 
-def create_dir(path):
+def create_dir(path: str) -> bool:
     if can_be_created(path):
         os.mkdir(path)
+        return True
+    return False
 
 
 def name_file(path_file: str) -> str:

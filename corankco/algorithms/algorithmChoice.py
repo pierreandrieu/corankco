7ef11a135206b3bb9fcc8ff1d140b779df40390f
andrieu <pierre.andrieu@lri.fr>
2023-07-01 19:57:39 +0200

    * remove "alltied" algorithm * add docstring * add types

--- corankco/algorithms/algorithmChoice.py
+++ corankco/algorithms/algorithmChoice.py
@@ -1,5 +1,5 @@
 from enum import Enum, unique
-from typing import Dict
+from typing import Dict, List
 from corankco.algorithms.median_ranking import MedianRanking
 from corankco.algorithms.bioconsert.bioconsert import BioConsert
 from corankco.algorithms.parcons.parcons import ParCons
@@ -11,10 +11,10 @@ from corankco.algorithms.medrank.medrank import MedRank
 from corankco.algorithms.borda.borda import BordaCount
 from corankco.algorithms.bioconsert.bioco import BioCo
 from corankco.algorithms.copeland.copeland import CopelandMethod
-from corankco.algorithms.alltied.all_tied import AllTied
 
 
 class AlgorithmEnumeration:
+    """Contains a list of classes for all available ranking algorithms."""
     median_ranking_algorithms = [
         BioConsert,
         ParCons,
@@ -26,12 +26,12 @@ class AlgorithmEnumeration:
         BordaCount,
         BioCo,
         CopelandMethod,
-        AllTied
     ]
 
 
 @unique
 class Algorithm(Enum):
+    """Enum representing the available ranking algorithms."""
     BioConsert = 0
     ParCons = 1
     Exact = 2
@@ -45,18 +45,48 @@ class Algorithm(Enum):
     AllTied = 10
 
     @staticmethod
-    def get_all():
+    def get_all() -> List['Algorithm']:
+        """
+        Returns a list of all available algorithms.
+
+        :return: A List of all the available algorithms.
+        :rtype: List[Algorithm]
+        """
         return [Algorithm.AllTied, Algorithm.BioConsert, Algorithm.ParCons,
                 Algorithm.Exact, Algorithm.KwikSortRandom, Algorithm.RepeatChoice, Algorithm.PickAPerm,
                 Algorithm.MedRank, Algorithm.BordaCount, Algorithm.BioCo, Algorithm.CopelandMethod]
 
     @staticmethod
-    def get_all_compatible_with_any_scoring_scheme():
+    def get_all_compatible_with_any_scoring_scheme() -> List['Algorithm']:
+        """
+        Returns a list of algorithms that are compatible with any scoring scheme.
+
+        The returned algorithms have implementations that can handle any scoring scheme.
+
+        :return: A List of all the algorithms compatible with any scoring scheme.
+        :rtype: List[Algorithm]
+        """
         return [Algorithm.BioConsert, Algorithm.ParCons, Algorithm.Exact,
                 Algorithm.KwikSortRandom, Algorithm.CopelandMethod]
 
 
 def get_algorithm(alg: Algorithm, parameters: Dict = None) -> MedianRanking:
+    """
+    Returns an instance of the specified algorithm.
+
+    :param alg: The algorithm to instantiate. Must be an instance of the Algorithm enum.
+    :type alg: Algorithm
+    :param parameters: The parameters to pass to the algorithm's constructor. If None, an empty dict will be used.
+    :type parameters: Dict, optional
+    :return: An instance of the specified algorithm.
+    :rtype: MedianRanking
+    :raises TypeError: If alg is not an instance of Algorithm, or if parameters is not a dict.
+    """
+    if not isinstance(alg, Algorithm):
+        raise TypeError("alg must be an instance of Algorithm")
+    if parameters is not None and not isinstance(parameters, Dict):
+        raise TypeError("parameters must be a dict or None")
+
     if parameters is None:
         parameters = {}
     res = (AlgorithmEnumeration.median_ranking_algorithms[alg.value])(**parameters)

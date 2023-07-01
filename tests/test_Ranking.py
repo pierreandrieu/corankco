c01b1a2e2c49aa9c808450aa6695de93efb0b778
andrieu <pierre.andrieu@lri.fr>
2023-07-01 20:08:23 +0200

    * improved tests for Ranking

--- tests/test_Ranking.py
+++ tests/test_Ranking.py
@@ -7,6 +7,7 @@ import unittest
 class TestRanking(unittest.TestCase):
     def setUp(self):
         self.ranking = Ranking.from_list([{'3', '2'}, {'1'}])
+
     def test_init(self):
         # Test with disjoint sets
         buckets: List[Set[Element]] = [{Element('A'), Element('B')}, {Element('C')}]
@@ -18,6 +19,8 @@ class TestRanking(unittest.TestCase):
         buckets = [{Element('A'), Element('B')}, {Element('B'), Element('C')}]
         with self.assertRaises(ValueError):
             ranking = Ranking(buckets)
+            if ranking:
+                pass
 
     def test_str_init(self):
         # Test with disjoint sets
@@ -30,6 +33,8 @@ class TestRanking(unittest.TestCase):
         buckets = [{'A', 'B'}, {'B', 'C'}]
         with self.assertRaises(ValueError):
             ranking = Ranking.from_list(buckets)
+            if ranking:
+                pass
 
     def test_int_init(self):
         # Test with disjoint sets
@@ -42,6 +47,8 @@ class TestRanking(unittest.TestCase):
         buckets = [{1, 2}, {2, 3}]
         with self.assertRaises(ValueError):
             ranking = Ranking.from_list(buckets)
+            if ranking:
+                pass
 
     def test_positions(self):
         buckets: List[Set[Element]] = [{Element('A'), Element('B')}, {Element('C')}]
@@ -69,13 +76,20 @@ class TestRanking(unittest.TestCase):
 
     def test_domain(self):
         # Test for the domain method
-        self.assertEqual(sorted(list(self.ranking.domain())), sorted(list({'3', '2', '1'})))
+        self.assertEqual(sorted(list(self.ranking.domain)), sorted(list({'3', '2', '1'})))
 
     def test_nb_elements(self):
         # Test for the nb_elements method
-        self.assertEqual(self.ranking.nb_elements(), 3)
+        self.assertEqual(self.ranking.nb_elements, 3)
 
-
+    def test_ranking_getitem(self):
+        bucket1 = {1, 2, 3}
+        bucket2 = {4, 5, 6}
+        bucket3 = {7, 8, 9}
+        ranking = Ranking.from_list([bucket1, bucket2, bucket3])
+        assert ranking[0] == bucket1
+        assert ranking[1] == bucket2
+        assert ranking[2] == bucket3
 
 
 # To run the tests:

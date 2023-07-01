51174ad2fedadb39014a02c889ade766463e0f21
andrieu <pierre.andrieu@lri.fr>
2023-07-01 19:59:43 +0200

    * add docstring

--- corankco/algorithms/median_ranking.py
+++ corankco/algorithms/median_ranking.py
@@ -9,6 +9,14 @@ class ScoringSchemeNotHandledException(Exception):
 
 
 class MedianRanking:
+
+    """
+    The MedianRanking class serves as an interface for implementing consensus ranking algorithms. It defines
+    several methods that each consensus ranking algorithm should implement, including the computation of
+    consensus rankings, determining whether a given scoring scheme is relevant for incomplete rankings,
+    and benchmarking the consensus computation time. An algorithm implemented using this interface should
+    also provide a full name and a string representation of itself.
+    """
     def compute_consensus_rankings(
             self,
             dataset: Dataset,
@@ -17,23 +25,31 @@ class MedianRanking:
             bench_mode: bool = False
     ) -> Consensus:
         """
-        :param dataset: A dataset containing the rankings to aggregate
-        :type dataset: Dataset (class Dataset in package 'datasets')
-        :param scoring_scheme: The penalty vectors to consider
-        :type scoring_scheme: ScoringScheme (class ScoringScheme in package 'distances')
-        :param return_at_most_one_ranking: the algorithm should not return more than one ranking
-        :type return_at_most_one_ranking: bool
-        :param bench_mode: is bench mode activated. If False, the algorithm may return more information
-        :type bench_mode: bool
-        :return Consensus containing a list of rankings. If the algorithm is not able to provide multiple consensus,
-        or if return_at_most_one_ranking is True, a Consensus containing a single consensus ranking is returned.
-        :raise ScoringSchemeNotHandledException when the algorithm cannot compute the consensus because the
-        implementation of the algorithm does not fit with the scoring scheme
-        """
+        Calculate and return the consensus rankings based on the given dataset and scoring scheme.
 
+        :param dataset: The dataset of rankings to be aggregated.
+        :type dataset: Dataset
+        :param scoring_scheme: The scoring scheme to be used for calculating consensus.
+        :type scoring_scheme: ScoringScheme
+        :param return_at_most_one_ranking: If True, the algorithm should return at most one ranking.
+        :type return_at_most_one_ranking: bool
+        :param bench_mode: If True, the algorithm may return additional information for benchmarking purposes.
+        :type bench_mode: bool
+        :return: Consensus rankings. If the algorithm is unable to provide multiple consensuses or
+        return_at_most_one_ranking is True, a single consensus ranking is returned.
+        :rtype: Consensus
+        :raise ScoringSchemeNotHandledException: When the algorithm cannot compute the consensus because the
+        implementation does not support the given scoring scheme.
+        """
         raise NotImplementedError("The method not implemented")
 
     def get_full_name(self) -> str:
+        """
+        Get the full name of the algorithm.
+
+        :return: The full name of the algorithm.
+        :rtype: str
+        """
         raise NotImplementedError("The method not implemented")
 
     def bench_time_consensus(self,
@@ -41,6 +57,20 @@ class MedianRanking:
                              scoring_scheme: ScoringScheme,
                              return_at_most_one_ranking: bool = False,
                              lower_bound_time: float = 2.) -> float:
+        """
+        Calculate and return the average computation time for a given dataset and scoring scheme.
+
+        :param dataset: The dataset of rankings to be aggregated.
+        :type dataset: Dataset
+        :param scoring_scheme: The scoring scheme to be used for calculating consensus.
+        :type scoring_scheme: ScoringScheme
+        :param return_at_most_one_ranking: If True, the algorithm should return at most one ranking.
+        :type return_at_most_one_ranking: bool
+        :param lower_bound_time: The lower bound on the total computation time.
+        :type lower_bound_time: float
+        :return: The average computation time.
+        :rtype: float
+        """
         sum_time = 0
         nb_computation = 0
         while sum_time <= lower_bound_time:
@@ -53,9 +83,21 @@ class MedianRanking:
 
     def is_scoring_scheme_relevant_when_incomplete_rankings(self, scoring_scheme: ScoringScheme) -> bool:
         """
-        :return: a list of distances from distance_enumeration
+        Determine whether the provided scoring scheme is relevant when dealing with incomplete rankings.
+
+        :param scoring_scheme: The scoring scheme to be evaluated.
+        :type scoring_scheme: ScoringScheme
+        :return: True if the scoring scheme is relevant for incomplete rankings, False otherwise.
+        :rtype: bool
         """
         raise NotImplementedError("The method not implemented")
 
     def __repr__(self):
+        """
+        Return a string representation of the object, including the class name and all instance variables.
+        The instance variables can be specific parameters for a given MedianRanking algorithm
+
+        :return: A string representation of the object.
+        :rtype: str
+        """
         return self.__class__.__name__ + " " + str(self.__dict__)

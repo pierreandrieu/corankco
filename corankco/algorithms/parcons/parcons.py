0f71b346f8e3fbb8bea4d82c0857934e0015c895
andrieu <pierre.andrieu@lri.fr>
2023-07-07 02:32:12 +0200

    improve readability and docstring

--- corankco/algorithms/parcons/parcons.py
+++ corankco/algorithms/parcons/parcons.py
@@ -3,15 +3,16 @@ from corankco.dataset import Dataset
 from corankco.scoringscheme import ScoringScheme
 from corankco.consensus import Consensus, ConsensusFeature
 from corankco.algorithms.bioconsert.bioconsert import BioConsert
-from typing import Tuple, List, Set, Dict
+from typing import List, Set
 from itertools import combinations
-from igraph import Graph, VertexClustering
+from igraph import VertexClustering
 from corankco.element import Element
-from numpy import vdot, ndarray, count_nonzero, shape, array, zeros, asarray
+from numpy import ndarray, asarray
 from corankco.ranking import Ranking
+from corankco.algorithms.graphbasedalgorithm import GraphBasedAlgorithm
 
 
-class ParCons(MedianRanking):
+class ParCons(GraphBasedAlgorithm):
     """
     ParCons is a graph-based heuristics for Kemeny-Young rank aggregation published in P. Andrieu, B. Brancotte,
     L. Bulteau, S. Cohen-Boulakia, A. Denise, A. Pierrot, S. Vialette, Efficient, robust and effective rank aggregation
@@ -25,7 +26,9 @@ class ParCons(MedianRanking):
     Note that this heuristics may be aware of having an optimal solution. If no auxiliary heuristics has been used for a
     given instance, the returned consensus is necessarily optimal.
     """
-    def __init__(self, auxiliary_algorithm: MedianRanking = None, bound_for_exact=80):
+    DEFAULT_BOUND_FOR_EXACT = 80
+
+    def __init__(self, auxiliary_algorithm: MedianRanking = None, bound_for_exact: int = None):
         """
         Construct a ParCons instance
         :param auxiliary_algorithm: the rank aggregation algorithm (MedianRanking instance) to use for
@@ -38,6 +41,11 @@ class ParCons(MedianRanking):
         else:
             self._auxiliary_alg: MedianRanking = BioConsert(starting_algorithms=None)
         self._bound_for_exact: int = bound_for_exact
+
+        if bound_for_exact is None:
+            self._bound_for_exact = self.DEFAULT_BOUND_FOR_EXACT
+        else:
+            self._bound_for_exact = bound_for_exact
 
     def compute_consensus_rankings(
             self,
@@ -74,69 +82,39 @@ class ParCons(MedianRanking):
         # numpy version of scoring scheme
         sc: ndarray = asarray(scoring_scheme.penalty_vectors)
 
-        rankings: List[Ranking] = dataset.rankings
         res: List[Set[Element]] = []
-
-        # mappings between elements and int IDs
-        elem_id: Dict[Element, int] = dataset.mapping_elem_id
-        id_elements: Dict[int, Element] = dataset.mapping_id_elem
 
         # positions[i][j] = position of element of id i in ranking j, -1 if non-ranked
         positions: ndarray = dataset.get_positions()
 
         # get the graph of elements and the cost matrix
-        gr1, mat_score = self.__graph_of_elements(positions, sc)
+        gr1, mat_score = ParCons._graph_of_elements(positions, sc)
 
         # get the strongly connected components in a topological sort
         scc: VertexClustering = gr1.components()
         
         # for each SCC (defining a sub-problem)
         for scc_i in scc:
-            # if scc = single element, the solution is trivial, a bucket with this element is added in the consensus
-            if len(scc_i) == 1:
-                bucket: Set[Element] = {id_elements.get(scc_i[0])}
-                res.append(bucket)
+            set_current_scc: Set[int] = set(scc_i)
+            set_current_elements: Set[Element] = {dataset.mapping_id_elem[el_scc] for el_scc in set_current_scc}
+            if ParCons._can_be_all_tied(set_current_scc, mat_score):
+                res.append(set_current_elements)
+            # if there is at least one pair of elements that cannot be tied with minimal cost,
+            # then we have no trivial optimal solution. According to the size of the sub-problem, use of
+            # the exact algorithm or a heuristics
             else:
-                # if scc has several elements but all pairs have tied cost as minimal cost,
-                # a trivial solution is to tie them in the consensus: a bucket containing all these elements is added
-                all_tied = True
-                for e1, e2 in combinations(scc_i, 2):
-                    if mat_score[e1][e2][2] > mat_score[e1][e2][0] or mat_score[e1][e2][2] > mat_score[e1][e2][1]:
-                        all_tied = False
-                        break
-                if all_tied:
-                    buck: Set[Element] = set()
-                    for el in scc_i:
-                        buck.add(id_elements.get(el))
-                    res.append(buck)
-
-                # if there is at least one pair of elements that cannot be tied with minimal cost,
-                # then we have no trivial optimal solution. According to the size of the sub-problem, use of
-                # the exact algorithm or a heuristics
+                # creation of a new Dataset representing the sub-problem
+                sub_problem = dataset.sub_problem_from_elements(set_current_elements)
+                if len(scc_i) > self._bound_for_exact:
+                    cons_ext = self._auxiliary_alg.compute_consensus_rankings(
+                        sub_problem, scoring_scheme, True).consensus_rankings[0]
+                    res.extend(cons_ext)
+                    optimal = False
                 else:
-                    # creation of a new Dataset representing the sub-problem
-                    set_scc = set(scc_i)
-                    project_rankings: List[Ranking] = []
-                    for ranking in rankings:
-                        project_ranking: List[Set[Element]] = []
-                        for bucket in ranking:
-                            project_bucket: Set[Element] = set(elem for elem in bucket if elem_id[elem] in set_scc)
-                            if len(project_bucket) > 0:
-                                project_ranking.append(project_bucket)
-                        if len(project_ranking) > 0:
-                            project_rankings.append(Ranking(project_ranking))
-                    if len(scc_i) > self._bound_for_exact:
-                        cons_ext = self._auxiliary_alg.compute_consensus_rankings(Dataset(project_rankings),
-                                                                                 scoring_scheme,
-                                                                                 True).consensus_rankings[0]
-                        res.extend(cons_ext)
-                        optimal = False
-                    else:
-                        cons_ext = ExactAlgorithm(preprocess=False).compute_consensus_rankings(
-                            Dataset(project_rankings),
-                            scoring_scheme,
-                            True).consensus_rankings[0]
-                        res.extend(cons_ext)
+                    cons_ext = ExactAlgorithm(preprocess=False).compute_consensus_rankings(
+                        sub_problem, scoring_scheme, True).consensus_rankings[0]
+                    res.extend(cons_ext)
+
         hash_information = {ConsensusFeature.IsNecessarilyOptimal: optimal,
                             ConsensusFeature.AssociatedAlgorithm: self.get_full_name()
                             }
@@ -149,71 +127,48 @@ class ParCons(MedianRanking):
                          att=hash_information)
 
     @staticmethod
-    def __graph_of_elements(positions: ndarray, matrix_scoring_scheme: ndarray) -> Tuple[Graph, ndarray]:
-        graph_of_elements: Graph = Graph(directed=True)
-        cost_before: ndarray = matrix_scoring_scheme[0]
-        cost_tied: ndarray = matrix_scoring_scheme[1]
-        cost_after: ndarray = array([cost_before[1], cost_before[0], cost_before[2], cost_before[4], cost_before[3],
-                            cost_before[5]])
+    def _can_be_all_tied(id_elements_to_check: Set[int], cost_matrix: ndarray) ->bool:
+        """
+        Check if all elements in a given set can be tied together with minimal cost.
 
-        # n = nb of elements, m = nb of rankings
-        n: int = shape(positions)[0]
-        m: int = shape(positions)[1]
+        This method goes through all pairs of elements in the given set, checking the cost of tying them together.
+        If the cost of tying any pair is higher than placing one before or after the other, the function returns False,
+        indicating that not all elements in the set can be tied together with minimal cost.
 
-        # add a vertex for each element
-        for i in range(n):
-            graph_of_elements.add_vertex(name=str(i))
-
-        # matrix[i][j] contains the costs of the 3 possible relative orderings between i and j
-        # matrix[i][j][0] = cost to place i before j, [1] = cost to place i after j, [2] = cost to tie i and j
-        matrix: ndarray = zeros((n, n, 3))
-
-        # arcs to add in the graph. There is an arc from x to y in the graph of elements iif the cost to place
-        # y before x in the consensus is NOT minimal. Intuitively, an arc from x to y mean "not after"
-        edges = []
-
-        for e1 in range(n):
-            # memoization: the positions of e1 in the input rankings
-            mem: ndarray = positions[e1]
-
-            # d = number of input rankings such that e1 is non-ranked
-            d: int = count_nonzero(mem == -1)
-
-            for e2 in range(e1 + 1, n):
-                # a : nb of input rankings such that e1 and e2 both non-ranked
-                a: int = count_nonzero(mem + positions[e2] == -2)
-                # b = nb of input rankings such that e1 and e2 have same position, or both non-ranked
-                b: int = count_nonzero(mem == positions[e2])
-                # c = nb of input rankings such that e2 is non-ranked
-                c: int = count_nonzero(positions[e2] == -1)
-                # e = nb of input rankings such that e1 < e2 or e1 is non-ranked
-                e: int = count_nonzero(mem < positions[e2])
-
-                # vector that contains for the two elements x and y the number of rankings such that respectively:
-                # x < y, x > y, x and y are tied, x is the only ranked, y is the only ranked, x and y are non-ranked
-                relative_positions: ndarray = array([e - d + a, m - e - b - c + a, b - a, c - a, d - a, a])
-
-                # cost to place e1 before, after, or tied with e2 in a consensus ranking within a Kemeny prism
-                put_before: float = vdot(relative_positions, cost_before)
-                put_after: float = vdot(relative_positions, cost_after)
-                put_tied: float = vdot(relative_positions, cost_tied)
-
-                # if x should not be placed after y, then arc from x to y
-                if put_before > put_after or put_before > put_tied:
-                    edges.append((e2, e1))
-                # if y should not be placed after x, then arc from y to x
-                if put_after > put_before or put_after > put_tied:
-                    edges.append((e1, e2))
-
-                # save the costs, will be used further
-                matrix[e1][e2] = [put_before, put_after, put_tied]
-                matrix[e2][e1] = [put_after, put_before, put_tied]
-        graph_of_elements.add_edges(edges)
-        return graph_of_elements, matrix
+        :param id_elements_to_check: a set of IDs of the elements to be checked.
+        :type id_elements_to_check: Set[int]
+        :param cost_matrix: a 3D matrix where cost_matrix[i][j][k] denotes the cost of placing i and j in
+                            k-th relative position in the consensus.
+        :type cost_matrix: ndarray
+        :return: True if all elements can be tied together with minimal cost, False otherwise.
+        :rtype: bool
+        """
+        if len(id_elements_to_check) < 2:
+            return True
+        for e1, e2 in combinations(id_elements_to_check, 2):
+            cost_to_tie = cost_matrix[e1][e2][2]
+            cost_to_place_before = cost_matrix[e1][e2][0]
+            cost_to_place_after = cost_matrix[e1][e2][1]
+            if cost_to_tie > min(cost_to_place_before, cost_to_place_after):
+                return False
+        return True
 
     def get_full_name(self) -> str:
-        return "ParCons, uses  \"" + self._auxiliary_alg.get_full_name() + "\" on groups of size >  " + \
+        """
+
+        :return: the name of the Algorithm i.e. "ParCons, uses " + the name of the auxiliary algorithm + " on
+        subproblems of size > " + the value of the associated parameter
+        """
+        return "ParCons, uses  \"" + self._auxiliary_alg.get_full_name() + "\" on subproblems of size >  " + \
             str(self._bound_for_exact)
 
     def is_scoring_scheme_relevant_when_incomplete_rankings(self, scoring_scheme: ScoringScheme) -> bool:
-        return True
+        """
+        Check if the scoring scheme is relevant when the rankings are incomplete.
+
+        :param scoring_scheme: The scoring scheme to be checked.
+        :type scoring_scheme: ScoringScheme
+        :return: True iif the auxiliary algorithm is compatible with the scoring scheme
+        :rtype: bool
+        """
+        return self._auxiliary_alg.is_scoring_scheme_relevant_when_incomplete_rankings(scoring_scheme)

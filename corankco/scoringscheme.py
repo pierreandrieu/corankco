e9f8cf8748415e8fd4b0cc113b296c7214c26c63
andrieu <pierre.andrieu@lri.fr>
2023-07-08 21:12:01 +0200

    force to use lists for the penalty vectors and add check of constraints

--- corankco/scoringscheme.py
+++ corankco/scoringscheme.py
@@ -1,18 +1,41 @@
-from typing import List, Union, Tuple
+from typing import List
 from math import isnan
-from numpy import ndarray
 
 
 class InvalidScoringScheme(Exception):
-    pass
+    def __init__(self,
+                 message="Scoring scheme must be 2 Lists. "
+                         "Each list must be a list of 6 real >= 0 values. "
+                         "For instance, [[0., 1., 1., 0., 0., 0.], [1., 1., 0., 1., 1., 0.]]"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NonRealPositiveValuesScoringScheme(Exception):
+    def __init__(self, message="Each list must be a list of 6 real >= 0 values. "
+                               "For instance, [[0., 1., 1., 0., 0., 0.], [1., 1., 0., 1., 1., 0.]]"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ForbiddenAssociationPenaltiesScoringScheme(Exception):
+    def __init__(self,
+                 message="The first value of the first List must be 0. "
+                         "The second value of the first List must be > 0"
+                         "The fourth value of the first List must be <= the fifth value of the second List"
+                         "The first and second values of the second List must be equal. "
+                         "The fourth and the fifth values of the second List must be equal"
+                         "The third value of the second List must be > 0"
+                         "For instance, [[0., 1., 1., 0., 0., 0.], [1., 1., 0., 1., 1., 0.]]"):
+        self.message = message
+        super().__init__(self.message)
 
 
 class ScoringScheme:
     """
     The ScoringScheme class represents a scoring scheme for comparing rankings.
 
-    :param penalties: A list, tuple, or ndarray of 2 penalty vectors i.e. list, tuple, or ndarray of size 6.
-    Default is [[0., 1., 1., 0., 1., 0.], [1., 1., 0., 1., 1., 0.]].
+    :param penalties: A list of 2 penalty vectors i.e. rwo lists of size 6.
 
     Let B be the first penalty vector and T be the second one, c be a consensus ranking (complete) and r be a ranking.
     - B: costs to pay for each elements x,y such that x < y in the consensus, more precisely:
@@ -30,51 +53,56 @@ class ScoringScheme:
       - T[4] if y is ranked and x is not
       - T[5] if x and y are non-ranked
 
-    :raises InvalidScoringScheme: If penalties is not of a correct format.
+    :raises InvalidScoringScheme: If penalties is not of a correct format
+    :raises NonRealPositiveValuesScoringScheme if one penalty is not a real value>= 0
+    :raises ForbiddenAssociationPenaltiesScoringScheme if one of the following is not respected:
+    - first value of first list must be 0
+    - second value of the first List must be > 0"
+    - fourth value of the first List must be <= the fifth value of the second List"
+    - third value of second list must be > 0
+    - first and second values of the second List must be equal
+    - fourth and fifth values of the second List must be equal
+
     """
 
-    def __init__(self, penalties: Union[List, Tuple, ndarray] = None):
+    def __init__(self, penalties: List[List[float]]):
         """
         Initialize the ScoringScheme object.
 
-        :param penalties: A list, tuple, or ndarray of penalty vectors, default is None, that is
-        [[0., 1., 1., 0., 1., 0.], [1., 1., 0., 1., 1., 0.]]
-        :rtype: Union[List, Tuple, ndarray]
+        :param penalties: A list of penalty vectors
+        :rtype: List
         :raises InvalidScoringScheme: If penalties is not of a correct format.
         """
-        if penalties is None:
-            self.__penalty_vectors: List[List[float]] = [[0., 1., 1., 0., 1., 0.], [1., 1., 0., 1., 1., 0.]]
-        else:
-            if type(penalties) is not list and type(penalties) is not tuple and type(penalties) is not ndarray:
-                raise InvalidScoringScheme("Scoring scheme must be 2 List or Tuple or ndarray of 6 reals each")
-            if len(penalties) != 2:
-                raise InvalidScoringScheme("Scoring scheme must be 2 List or Tuple or ndarray of 6 reals each")
-            if type(penalties[0]) is not list and type(penalties[0]) is not tuple and type(penalties[0]) is not ndarray:
-                raise InvalidScoringScheme("Scoring scheme must be 2 List or Tuple or ndarray of 6 reals each")
-            if type(penalties[1]) is not list and type(penalties[1]) is not tuple and type(penalties[1]) is not ndarray:
-                raise InvalidScoringScheme("Scoring scheme must be 2 List or Tuple or ndarray of 6 reals each")
-            if len(penalties[0]) != 6 or len(penalties[1]) != 6:
-                raise InvalidScoringScheme("Scoring scheme must be 2 List or Tuple or ndarray of 6 reals each")
-            penalties_copy: List[List[float]] = [[], []]
-            for pen in penalties[0]:
-                pen_split_point = decimal_part = str(pen).split(".")
-                if len(pen_split_point) != 2:
-                    raise InvalidScoringScheme("Coefficients must be real values")
-                if not pen_split_point[0].isdigit() or not pen_split_point[1].isdigit():
-                    raise InvalidScoringScheme("Coefficients must be real values")
-                if float(pen) < 0:
-                    raise InvalidScoringScheme("Coefficients must be >= 0")
+        # check types and lengths
+        if type(penalties) is not list or len(penalties) != 2:
+            raise InvalidScoringScheme()
+        if type(penalties[0]) is not list or type(penalties[1]) is not list:
+            raise InvalidScoringScheme()
+        if len(penalties[0]) != 6 or len(penalties[1]) != 6:
+            raise InvalidScoringScheme()
 
-                penalties_copy[0].append(float(pen))
-            for pen in penalties[1]:
-                penalties_copy[1].append(float(pen))
-                if float(pen) < 0:
-                    raise InvalidScoringScheme("Coefficients must be >= 0")
-            if penalties_copy[1][0] != penalties_copy[1][1] or penalties_copy[1][3] != penalties_copy[1][4]:
-                raise InvalidScoringScheme("Must have T1 = T2 and T4 = T5")
-            if penalties_copy[0][0] > 0 or penalties_copy[1][2] > 0:
-                raise InvalidScoringScheme("Must have B[1] = 0")
-            self.__penalty_vectors = penalties_copy
+        # copy the penalties in a new List of List of floats
+        penalties_copy: List[List[float]] = [[], []]
+        for pen in penalties[0]:
+            if (not isinstance(pen, float) and not isinstance(pen, int)) or pen < 0:
+                raise NonRealPositiveValuesScoringScheme()
+            penalties_copy[0].append(float(pen))
+        for pen in penalties[1]:
+            if (not isinstance(pen, float) and not isinstance(pen, int)) or pen < 0:
+                raise NonRealPositiveValuesScoringScheme()
+            penalties_copy[1].append(float(pen))
+
+        # check assertions
+        if penalties_copy[1][0] != penalties_copy[1][1] or penalties_copy[1][3] != penalties_copy[1][4] \
+                or penalties_copy[0][3] > penalties_copy[0][4]:
+            raise ForbiddenAssociationPenaltiesScoringScheme()
+        if penalties_copy[0][0] > 0 or penalties_copy[1][2] > 0:
+            raise ForbiddenAssociationPenaltiesScoringScheme()
+        if penalties_copy[0][3] > penalties_copy[0][4]:
+            raise ForbiddenAssociationPenaltiesScoringScheme()
+        if penalties_copy[0][1] == 0:
+            raise ForbiddenAssociationPenaltiesScoringScheme()
+        self._penalty_vectors = penalties_copy
 
     @property
     def b1(self) -> float:
@@ -84,7 +112,7 @@ class ScoringScheme:
         :return: The first element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][0]
+        return self._penalty_vectors[0][0]
 
     @property
     def b2(self) -> float:
@@ -94,7 +122,7 @@ class ScoringScheme:
         :return: The second element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][1]
+        return self._penalty_vectors[0][1]
 
     @property
     def b3(self) -> float:
@@ -104,7 +132,7 @@ class ScoringScheme:
         :return: The third element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][2]
+        return self._penalty_vectors[0][2]
 
     @property
     def b4(self) -> float:
@@ -113,7 +141,7 @@ class ScoringScheme:
         :return: The fourth element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][3]
+        return self._penalty_vectors[0][3]
 
     @property
     def b5(self) -> float:
@@ -122,7 +150,7 @@ class ScoringScheme:
         :return: The fifth element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][4]
+        return self._penalty_vectors[0][4]
 
     @property
     def b6(self) -> float:
@@ -131,7 +159,7 @@ class ScoringScheme:
         :return: The sixth element of the first penalty vector B.
         :rtype: float
         """
-        return self.__penalty_vectors[0][5]
+        return self._penalty_vectors[0][5]
 
     @property
     def t1_and_t2(self) -> float:
@@ -140,7 +168,7 @@ class ScoringScheme:
         :return: The first/second element of the first penalty vector T.
         :rtype: float
         """
-        return self.__penalty_vectors[1][0]
+        return self._penalty_vectors[1][0]
 
     @property
     def t3(self) -> float:
@@ -149,7 +177,7 @@ class ScoringScheme:
         :return: The third element of the first penalty vector T.
         :rtype: float
         """
-        return self.__penalty_vectors[1][2]
+        return self._penalty_vectors[1][2]
 
     @property
     def t4_and_t5(self) -> float:
@@ -158,7 +186,7 @@ class ScoringScheme:
         :return: The fourth/fifth element of the first penalty vector T.
         :rtype: float
         """
-        return self.__penalty_vectors[1][3]
+        return self._penalty_vectors[1][3]
 
     @property
     def t6(self) -> float:
@@ -167,16 +195,17 @@ class ScoringScheme:
         :return: The sixth element of the first penalty vector T.
         :rtype: float
         """
-        return self.__penalty_vectors[1][5]
+        return self._penalty_vectors[1][5]
 
     @property
     def penalty_vectors(self) -> List[List[float]]:
         """
         Returns the List of two penalty vectors (each one is a List of 6 floats) of the ScoringScheme
-        :return: List of two penalty vectors (each one is a List of 6 floats) of the ScoringScheme. For instance, [[0., 1., 0.5, 0., 1., 0.], [0.5, 0.5, 0., 0.5, 0.5, 0.]]
+        :return: List of two penalty vectors (each one is a List of 6 floats) of the ScoringScheme.
+        For instance, [[0., 1., 0.5, 0., 1., 0.], [0.5, 0.5, 0., 0.5, 0.5, 0.]]
         :rtype: List[List[float]]
         """
-        return self.__penalty_vectors
+        return self._penalty_vectors
 
     @property
     def b_vector(self) -> List[float]:
@@ -202,7 +231,7 @@ class ScoringScheme:
         :return: a string view of the ScoringScheme
         :rtype: str
         """
-        return str(self.__penalty_vectors)
+        return str(self._penalty_vectors)
 
     def __repr__(self) -> str:
         """
@@ -210,7 +239,35 @@ class ScoringScheme:
         :return: a string view of the ScoringScheme
         :rtype: str
         """
-        return str(self.__penalty_vectors)
+        return str(self._penalty_vectors)
+
+    def __mul__(self, other: float) -> 'ScoringScheme':
+        """
+        Defines the behavior of multiplying a ScoringScheme by a float.
+        Multiplies all penalties in the ScoringScheme by the given integer.
+
+        :param other: An integer to multiply the ScoringScheme by.
+        :type other: int
+        :return: A new ScoringScheme with all penalties multiplied by 'other'.
+        :rtype: ScoringScheme
+        """
+        if not isinstance(other, float) and not isinstance(other, int):
+            raise ValueError("Can only multiply by an integer or a float number")
+        new_penalties: List[List[float]] = [[pen * float(other) for pen in penalty_vector]
+                                            for penalty_vector in self._penalty_vectors]
+        return ScoringScheme(new_penalties)
+
+    def __rmul__(self, other: float) -> 'ScoringScheme':
+        """
+        Defines the behavior of multiplying a float by a ScoringScheme.
+        Calls the __mul__ function to handle the operation.
+
+        :param other: An integer to multiply the ScoringScheme by.
+        :type other: int
+        :return: A new ScoringScheme with all penalties multiplied by 'other'.
+        :rtype: ScoringScheme
+        """
+        return self.__mul__(other)
 
     def description(self) -> str:
         """
@@ -228,20 +285,21 @@ class ScoringScheme:
             f"\t\tx missing y present ranking: {self.b5}\n"
             f"\t\tx and y missing in input ranking: {self.b6}\n"
             f"\t\tx and y tied in consensus\n"
-            f"\t\tx before y in input ranking: {self.__t1}\n"
-            f"\t\ty before x in input ranking: {self.t2}\n"
+            f"\t\tx before y in input ranking: {self.t1_and_t2}\n"
+            f"\t\ty before x in input ranking: {self.t1_and_t2}\n"
             f"\t\tx and y tied in input ranking: {self.t3}\n"
-            f"\t\tx present y missing in input ranking: {self.t4}\n"
-            f"\t\tx missing y present ranking: {self.t5}\n"
+            f"\t\tx present y missing in input ranking: {self.t4_and_t5}\n"
+            f"\t\tx missing y present ranking: {self.t4_and_t5}\n"
             f"\t\tx and y missing in input ranking: {self.t6}\n"
         )
         return desc
 
     @staticmethod
-    def get_pseudodistance_scoring_scheme():
+    def get_pseudodistance_scoring_scheme() -> 'ScoringScheme':
         """
         Get the pseudo-distance defined in:
-        Brancotte, Bryan & Rance, Bastien & Denise, Alain & Cohen-Boulakia, Sarah. (2014). ConQuR-Bio: Consensus Ranking with Query Reformulation for Biological Data. 10.1007/978-3-319-08590-6_13. .
+        Brancotte, Bryan & Rance, Bastien & Denise, Alain & Cohen-Boulakia, Sarah. (2014). ConQuR-Bio: Consensus Ranking
+        with Query Reformulation for Biological Data. 10.1007/978-3-319-08590-6_13. .
 
         :return: The ScoringScheme [[0., 1., 1., 0., 1., 0.], [1., 1., 0., 1., 1., 0.]]
         """
@@ -265,7 +323,8 @@ class ScoringScheme:
     def get_pseudodistance_scoring_scheme_p(p: float):
         """
         Get the pseudo-distance defined in:
-        Brancotte, Bryan & Rance, Bastien & Denise, Alain & Cohen-Boulakia, Sarah. (2014). ConQuR-Bio: Consensus Ranking with Query Reformulation for Biological Data. 10.1007/978-3-319-08590-6_13. .
+        Brancotte, Bryan & Rance, Bastien & Denise, Alain & Cohen-Boulakia, Sarah. (2014).
+        ConQuR-Bio: Consensus Ranking with Query Reformulation for Biological Data. 10.1007/978-3-319-08590-6_13
         with reap parameter p = cost of creating / breaking ties
         :param p: the cost to create / break ties
         :return: The ScoringScheme [[0., 1., 1., 0., 1., 0.], [1., 1., 0., 1., 1., 0.]]
@@ -300,10 +359,6 @@ class ScoringScheme:
         """
         return ScoringScheme(penalties=[[0., 1., 0., 0., 0., 0.], [1., 1., 0., 1., 1., 1.]])
 
-    @staticmethod
-    def get_fagin_score_for_complete_ranking(p: float):
-        return ScoringScheme(penalties=[[0., 1., p, 0., 0., 0.], [p, p, 0., 0., 0., 0.]])
-
     def is_equivalent_to(self, other) -> bool:
         """
         Check if the current scoring scheme is equivalent to the given scoring scheme.
@@ -322,17 +377,17 @@ class ScoringScheme:
         """
         return self.__is_equivalent_to_generic(other, 3)
 
-    def __is_equivalent_to_generic(self, other, stop: int) -> bool:
+    def __is_equivalent_to_generic(self, other: 'ScoringScheme', stop: int) -> bool:
         """
-        Check if the current scoring scheme is equivalent to the given scoring scheme considering only ranges 0...stop-1.
+        Check if current scoring scheme is equivalent to the given scoring scheme considering only ranges 0...stop-1.
 
         :param other: A ScoringScheme object.
         :param stop: the range of each penalty score to compare
         :return: True if the scoring schemes are equivalent, False otherwise.
         """
-        pen2 = other.__penalty_vectors
-        pen1 = self.__penalty_vectors
-        coefficient = float("nan")
+        pen2: List[List[float]] = other.penalty_vectors
+        pen1: List[List[float]] = self._penalty_vectors
+        coefficient: float = float("nan")
         for i in range(stop):
             if pen1[0][i] == 0:
                 if pen2[0][i] != 0:
@@ -365,4 +420,4 @@ class ScoringScheme:
         return self.__str__()
 
     def __getitem__(self, item: int) -> List[float]:
-        return self.__penalty_vectors[item]
+        return self._penalty_vectors[item]

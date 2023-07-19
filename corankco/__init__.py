807dd89e8640e6aa412a104b532d9b5b680bfe8d
andrieu <pierre.andrieu@lri.fr>
2023-07-20 01:45:07 +0200

    fix imports in __init__ files

--- corankco/__init__.py
+++ corankco/__init__.py
@@ -6,3 +6,5 @@ from .element import Element
 from .consensus import Consensus, ConsensusFeature
 from .utils import *
 from .kemeny_score_computation import KemenyComputingFactory, InvalidRankingsForComputingDistance
+from .partitioning import OrderedPartition
+from .algorithms import *
--- corankco/algorithms/__init__.py
+++ corankco/algorithms/__init__.py
@@ -5,3 +5,10 @@ Module that allows to choose a rank aggregation algorithm from an enumeration.
 from .algorithm_choice import get_algorithm, Algorithm, AlgorithmEnumeration
 from .pairwisebasedalgorithm import PairwiseBasedAlgorithm
 from .rank_aggregation_algorithm import RankAggAlgorithm
+from .exact import ExactAlgorithm
+from .borda import BordaCount
+from .copeland import CopelandMethod
+from .pickaperm import PickAPerm
+from .parcons import ParCons
+from .bioconsert import BioConsert, BioCo
+from .kwiksort import KwikSortRandom

e6ade7bc3c2c50469cc63fed7bf043a2a0f46c36
andrieu <pierre.andrieu@lri.fr>
2023-08-16 17:10:35 +0200

    more tests for bioconsert

--- tests/test_BioConsert.py
+++ tests/test_BioConsert.py
@@ -12,6 +12,7 @@ class TestBordaCount(unittest.TestCase):
         self.scoring_scheme_unifying = ScoringScheme.get_unifying_scoring_scheme()
         self.scoring_scheme_induced = ScoringScheme.get_induced_measure_scoring_scheme()
         self.scoring_scheme_pseudo = ScoringScheme.get_pseudodistance_scoring_scheme()
+        self.scoring_scheme_pseudo_05 = ScoringScheme.get_pseudodistance_scoring_scheme_p(0.5)
 
     def test_consensus_same_rankings(self):
         dataset = Dataset([Ranking([{1}, {2}, {3}])] * 3)
@@ -26,18 +27,12 @@ class TestBordaCount(unittest.TestCase):
     def test_consensus_more_than_one(self):
         dataset = Dataset([Ranking([{1}, {2}])] + [Ranking([{2}, {1}])])
 
-        consensus = self.my_alg.compute_consensus_rankings(dataset, ScoringScheme.get_pseudodistance_scoring_scheme_p(1.), False)
+        consensus = self.my_alg.compute_consensus_rankings(dataset, self.scoring_scheme_pseudo, False)
         self.assertEqual(len(consensus), 2)
 
-        consensus = self.my_alg.compute_consensus_rankings(dataset,
-                                                           ScoringScheme.get_pseudodistance_scoring_scheme_p(0.5), False)
+        consensus = self.my_alg.compute_consensus_rankings(dataset, self.scoring_scheme_pseudo_05, False)
         self.assertEqual(len(consensus), 3)
 
-
-    def test_consensus_different_rankings(self):
-        dataset = Dataset([Ranking([{1}, {2}, {3}])] * 2 + [Ranking([{3}, {2}, {1}])])
-        consensus = self.my_alg.compute_consensus_rankings(dataset, self.scoring_scheme_unifying)
-        self.assertEqual(str(consensus.consensus_rankings[0]), str(Ranking([{1}, {2}, {3}])))
 
 if __name__ == '__main__':
     unittest.main()

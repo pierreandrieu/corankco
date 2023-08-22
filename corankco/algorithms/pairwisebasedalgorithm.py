170e4cdf400349f22fb23f1527090e0b6a0b2095
andrieu <pierre.andrieu@lri.fr>
2023-08-22 14:21:37 +0200

    use of numba to speed up computation of pairwise cost matrix, written in C style for more readability

--- corankco/algorithms/pairwisebasedalgorithm.py
+++ corankco/algorithms/pairwisebasedalgorithm.py
@@ -2,11 +2,98 @@
 Module that implements generic functions about pairwise based rank aggregation algorithm. Module for code factorisation.
 """
 
-from typing import Tuple, List, Callable, Any, Set
+from typing import Tuple, Set
 from itertools import combinations
+from numba import jit
 from igraph import Graph
-from numpy import ndarray, shape, zeros, newaxis, sum as np_sum
+from numpy import ndarray, shape, zeros, asarray, logical_or, where, logical_and, ones, column_stack, newaxis
 from corankco.scoringscheme import ScoringScheme
+
+
+@jit("float64[:, :, :](int32[:, :], float64[:, :], float64[:], int32, int32)", nopython=True, cache=True)
+def _pairwise_cost_matrix_only(positions, scoring_scheme_numpy, weights, nb_elem, nb_rankings) -> ndarray:
+    """
+    Computes the pairwise cost matrix.
+
+    :param positions: The 2D ndarray matrix where positions[i][j] is the position of element int id i in ranking j
+    :param scoring_scheme_numpy: The associated scoring scheme, 2D numpy ndarray form.
+    :param weights: a float64 array that associates a weight for each ranking
+    :return: The pairwise cost matrix as n * n * 3 ndarray, where n is the number of elements
+    """
+
+    scoring_scheme_b_vector = scoring_scheme_numpy[0]
+    scoring_scheme_t_vector = scoring_scheme_numpy[1]
+
+    # create the matrix
+    matrix = zeros((nb_elem, nb_elem, 3))
+
+    # 2D float array, weighted_b_vector[i] = scoring_scheme_b_vector * weights[i]
+    # dimension of the array: nb_rankings, 6
+    weighted_b_vector = scoring_scheme_b_vector * weights[:, newaxis]
+    # same for t vector
+    weighted_t_vector = scoring_scheme_t_vector * weights[:, newaxis]
+
+    # fill the matrix
+    for elem1 in range(nb_elem):
+        # array : pos of element 1 in all rankings
+        all_pos_elem1 = positions[elem1]
+
+        # pointer save
+        cost_elem1 = matrix[elem1]
+        for elem2 in range(elem1 + 1, nb_elem):
+            # array : pos of element 2 in all rankings
+            all_pos_elem2 = positions[elem2]
+
+            cost_elem1_elem2 = cost_elem1[elem2]
+            for id_ranking in range(nb_rankings):
+                # pos of element 1 in ranking id_ranking
+                pos_elem1 = all_pos_elem1[id_ranking]
+                # pos of element 2 in ranking id_ranking
+                pos_elem2 = all_pos_elem2[id_ranking]
+
+                # if both elements are ranked in the target ranking
+                if pos_elem1 != -1 and pos_elem2 != -1:
+                    # elem1 before elem2
+                    if pos_elem1 < pos_elem2:
+                        cost_elem1_elem2[0] += weighted_b_vector[id_ranking][0]
+                        cost_elem1_elem2[1] += weighted_b_vector[id_ranking][1]
+                        cost_elem1_elem2[2] += weighted_t_vector[id_ranking][0]
+
+                    # elem2 before elem1
+                    elif pos_elem1 > pos_elem2:
+                        cost_elem1_elem2[0] += weighted_b_vector[id_ranking][1]
+                        cost_elem1_elem2[1] += weighted_b_vector[id_ranking][0]
+                        cost_elem1_elem2[2] += weighted_t_vector[id_ranking][1]
+
+                    # elem1 tied with elem2
+                    else:
+                        cost_elem1_elem2[0] += weighted_b_vector[id_ranking][2]
+                        cost_elem1_elem2[1] += weighted_b_vector[id_ranking][2]
+                        cost_elem1_elem2[2] += weighted_t_vector[id_ranking][2]
+                # only elem1 is ranked
+                elif pos_elem1 != -1:
+                    cost_elem1_elem2[0] += weighted_b_vector[id_ranking][3]
+                    cost_elem1_elem2[1] += weighted_b_vector[id_ranking][4]
+                    cost_elem1_elem2[2] += weighted_t_vector[id_ranking][3]
+
+                # only elem2 is ranked
+                elif pos_elem2 != -1:
+                    cost_elem1_elem2[0] += weighted_b_vector[id_ranking][4]
+                    cost_elem1_elem2[1] += weighted_b_vector[id_ranking][3]
+                    cost_elem1_elem2[2] += weighted_t_vector[id_ranking][4]
+
+                # elem1 and elem2 are non-ranked
+                else:
+                    cost_elem1_elem2[0] += weighted_b_vector[id_ranking][5]
+                    cost_elem1_elem2[1] += weighted_b_vector[id_ranking][5]
+                    cost_elem1_elem2[2] += weighted_t_vector[id_ranking][5]
+
+            # matrix is symmetric
+            matrix[elem2][elem1][0] = cost_elem1_elem2[1]
+            matrix[elem2][elem1][1] = cost_elem1_elem2[0]
+            matrix[elem2][elem1][2] = cost_elem1_elem2[2]
+
+    return matrix
 
 
 class PairwiseBasedAlgorithm:
@@ -16,66 +103,8 @@ class PairwiseBasedAlgorithm:
     """
 
     @staticmethod
-    def _pairwise_cost_matrix_generic(
-            positions: ndarray, scoring_scheme, callback: Callable[[ndarray, int, int, Any], Any], structure: Any) \
-            -> ndarray:
-        """
-        Computes the pairwise cost matrix, and allows for calling a function for each pair of elements after computing
-        the cost of the different relative positions. Useful for code factorisation.
-
-        :param positions: The 2D ndarray matrix where positions[i][j] is the position of element int id i in ranking j
-        :param scoring_scheme: The associated scoring scheme.
-        :param callback: The function to call after computing before(x,y), before(y,x), tied(x,y)
-        :param structure: A structure object, a parameter for the callback function.
-        :return: The pairwise cost matrix as n * n * 3 ndarray, where n is the number of elements
-        """
-
-        nb_elem, nb_rankings = positions.shape
-
-        # Create the matrix
-        matrix = zeros((nb_elem, nb_elem, 3))
-
-        # For each pair of elements el_1 and el_2
-        el_1_positions = positions[:, newaxis, :]
-        el_2_positions = positions[newaxis, :, :]
-
-        # Calculate each condition for relative positions
-        e1_before_e2 = np_sum((el_1_positions < el_2_positions) & (el_1_positions != -1) & (el_2_positions != -1),
-                              axis=2)
-        e2_before_e1 = np_sum((el_2_positions < el_1_positions) & (el_1_positions != -1) & (el_2_positions != -1),
-                              axis=2)
-        e1_e2_same_pos = np_sum((el_1_positions == el_2_positions) & (el_1_positions != -1), axis=2)
-        e1_present_e2_absent = np_sum((el_1_positions != -1) & (el_2_positions == -1), axis=2)
-        e2_present_e1_absent = np_sum((el_2_positions != -1) & (el_1_positions == -1), axis=2)
-        e1_e2_both_absent = np_sum((el_1_positions == -1) & (el_2_positions == -1), axis=2)
-
-        relative_positions = zeros((nb_elem, nb_elem, 6))
-        relative_positions[:, :, 0] = e1_before_e2
-        relative_positions[:, :, 1] = e2_before_e1
-        relative_positions[:, :, 2] = e1_e2_same_pos
-        relative_positions[:, :, 3] = e1_present_e2_absent
-        relative_positions[:, :, 4] = e2_present_e1_absent
-        relative_positions[:, :, 5] = e1_e2_both_absent
-
-        # Compute costs
-        cost_before = scoring_scheme.b_vector
-        cost_after = [cost_before[1], cost_before[0], cost_before[2], cost_before[4], cost_before[3],
-                      cost_before[5]]
-        cost_tied = scoring_scheme.t_vector
-
-        matrix[:, :, 0] = np_sum(relative_positions * cost_before, axis=2)
-        matrix[:, :, 1] = np_sum(relative_positions * cost_after, axis=2)
-        matrix[:, :, 2] = np_sum(relative_positions * cost_tied, axis=2)
-
-        for el_1 in range(nb_elem):
-            for el_2 in range(el_1 + 1, nb_elem):
-                callback(matrix[el_1][el_2], el_1, el_2, structure)
-
-        return matrix
-
-    @staticmethod
-    def graph_of_elements_with_robust_arcs(positions: ndarray, scoring_scheme: ScoringScheme) -> \
-            Tuple[Graph, ndarray, Set[Tuple[int, int]]]:
+    def graph_of_elements_with_robust_arcs(positions: ndarray, scoring_scheme: ScoringScheme,
+                                           weights: ndarray = None) -> Tuple[Graph, ndarray, Set[Tuple[int, int]]]:
         """
         Compute the graph of elements, the cost of pairwise relative positions and the set of robust arcs defined in the
         Future Generation Computer Systems article (as mentioned in the Class docstring)
@@ -87,27 +116,21 @@ class PairwiseBasedAlgorithm:
 
         :param positions: a matrix where pos[i][j] denotes the position of element i in ranking j (-1 if non-ranked)
         :param scoring_scheme: the scoring scheme to compute the cost matrix
+        :param weights: a 1D float array that associates a weight for each ranking
         :return: A tuple containing the Graph of elements defined in the FGCS article, the 3D matrix of costs of
         pairwise relative positions, and the set of the robust arcs defined in the FGCS article
         """
-        graph_of_elements: Graph = Graph(directed=True)
-        arcs: List[Tuple[int, int]] = []
-        robust_arcs: Set[Tuple[int, int]] = set()
-
-        # add a vertex for each element
-        for i in range(shape(positions)[0]):
-            graph_of_elements.add_vertex(name=str(i))
-
-        matrix: ndarray = PairwiseBasedAlgorithm._pairwise_cost_matrix_generic(
-            positions, scoring_scheme, PairwiseBasedAlgorithm._fill_graph_and_robust_arcs, (arcs, robust_arcs)
-        )
-
-        # arcs should be added all at once, the impact on performances is clear
-        graph_of_elements.add_edges(arcs)
-        return graph_of_elements, matrix, robust_arcs
+        if weights is None:
+            weights = ones(positions.shape[1], dtype=float)
+        assert weights.shape[0] == positions.shape[1]
+        pairwise_matrix: ndarray = PairwiseBasedAlgorithm.pairwise_cost_matrix(positions, scoring_scheme, weights)
+        return (PairwiseBasedAlgorithm._get_graph_of_elements_from_matrix(pairwise_matrix),
+                pairwise_matrix,
+                PairwiseBasedAlgorithm._get_robust_arcs_from_matrix(pairwise_matrix))
 
     @staticmethod
-    def graph_of_elements(positions: ndarray, scoring_scheme: ScoringScheme) -> Tuple[Graph, ndarray]:
+    def graph_of_elements(positions: ndarray, scoring_scheme: ScoringScheme,
+                          weights: ndarray = None) -> Tuple[Graph, ndarray]:
         """
         Compute the graph of elements, the cost of pairwise relative positions and the set of robust arcs defined in the
         Future Generation Computer Systems article (as mentioned in the Class docstring)
@@ -119,26 +142,19 @@ class PairwiseBasedAlgorithm:
 
         :param positions: a matrix where pos[i][j] denotes the position of element i in ranking j (-1 if non-ranked)
         :param scoring_scheme: the Scoring Scheme to compute the cost matrix
+        :param weights: a 1D float array that associates a weight for each ranking
         :return: A tuple containing the Graph of elements defined in the FGCS article, the 3D matrix of costs of
         pairwise relative positions
         """
-        graph_of_elements: Graph = Graph(directed=True)
+        if weights is None:
+            weights = ones(positions.shape[1], dtype=float)
+        assert weights.shape[0] == positions.shape[1]
 
-        # add a vertex for each element
-        for i in range(shape(positions)[0]):
-            graph_of_elements.add_vertex(name=str(i))
-
-        arcs: List[Tuple[int, int]] = []
-
-        matrix: ndarray = PairwiseBasedAlgorithm._pairwise_cost_matrix_generic(
-            positions, scoring_scheme, PairwiseBasedAlgorithm._fill_graph, arcs)
-
-        # arcs should be added all at once, the impact on performances is clear
-        graph_of_elements.add_edges(arcs)
-        return graph_of_elements, matrix
+        pairwise_matrix: ndarray = PairwiseBasedAlgorithm.pairwise_cost_matrix(positions, scoring_scheme, weights)
+        return PairwiseBasedAlgorithm._get_graph_of_elements_from_matrix(pairwise_matrix), pairwise_matrix
 
     @staticmethod
-    def pairwise_cost_matrix(positions: ndarray, scoring_scheme: ScoringScheme) -> ndarray:
+    def pairwise_cost_matrix(positions: ndarray, scoring_scheme: ScoringScheme, weights: ndarray = None) -> ndarray:
         """
         Compute the cost of pairwise relative positions.
 
@@ -148,76 +164,45 @@ class PairwiseBasedAlgorithm:
 
         :param positions: a matrix where pos[i][j] denotes the position of element i in ranking j (-1 if non-ranked)
         :param scoring_scheme: the scoring scheme to compute the cost matrix
+        :param weights: a 1D float array that associates a weight for each ranking
         :return: The 3D matrix of costs of pairwise relative positions.
         """
-        return PairwiseBasedAlgorithm._pairwise_cost_matrix_generic(
-            positions, scoring_scheme, PairwiseBasedAlgorithm._nothing_to_do, structure=None)
+        if weights is None:
+            weights = ones(positions.shape[1], dtype=float)
+        assert weights.shape[0] == positions.shape[1]
+        nb_elem = positions.shape[0]
+        nb_rankings = positions.shape[1]
+        return _pairwise_cost_matrix_only(positions, asarray(scoring_scheme.penalty_vectors),
+                                          weights, nb_elem, nb_rankings)
 
     @staticmethod
-    def _nothing_to_do(cost_matrix: ndarray, el_1: int, el_2: int, structure: None) -> None:
-        """
-        Function to be called by a callback when nothing should be done
-        :param cost_matrix: The 3D cost matrix
-        :param el_1: The 1st element.
-        :param el_2: The 2nd element.
-        :param structure: object, type Any
-        :return: None
-        """
+    def _get_robust_arcs_from_matrix(matrix: ndarray) -> Set[Tuple[int, int]]:
+        # pairs i,j where matrix[i][j][1] > matrix[i, j, 0] i.e. i before j cheaper than i after j
+        before_cheaper_after = matrix[:, :, 1] > matrix[:, :, 0]
+        # pairs i,j where matrix[i][j][2] > matrix[i, j, 0] i.e. i before j cheaper than i tied with j
+        before_cheaper_tied = matrix[:, :, 2] > matrix[:, :, 0]
+
+        return {(elem1, elem2) for elem1, elem2 in
+                column_stack(where(logical_and(before_cheaper_after, before_cheaper_tied)))}
 
     @staticmethod
-    def _fill_graph_and_robust_arcs(cost_matrix: ndarray, el_1: int, el_2: int,
-                                    arcs_robust_arcs: Tuple[List[Tuple[int, int]], Set[Tuple[int, int]]]) -> None:
-        """
-        Function to be called by a callback when the graph of elements must be computed,
-        with its set of robust arcs.
+    def _get_graph_of_elements_from_matrix(matrix: ndarray) -> Graph:
+        graph_of_elements: Graph = Graph(directed=True)
 
-        :param cost_matrix: The 3D cost matrix.
-        :param el_1: The 1st element.
-        :param el_2: The 2nd element.
-        :param arcs_robust_arcs: The list of arcs and the set of robust arcs to be modified when
-               computing the cost matrix, as a Tuple.
-        :return: None.
-        """
+        # add a vertex for each element
+        for i in range(shape(matrix)[0]):
+            graph_of_elements.add_vertex(name=str(i))
 
-        # if x should not be placed after y, then arc from x to y
-        put_before: float = cost_matrix[0]
-        put_after: float = cost_matrix[1]
-        put_tied: float = cost_matrix[2]
-        arcs: List[Tuple[int, int]] = arcs_robust_arcs[0]
-        robust_arcs: Set[Tuple[int, int]] = arcs_robust_arcs[1]
+        # pairs i,j where matrix[i][j][1] > matrix[i, j, 0] i.e. i before j cheaper than i after j
+        before_cheaper_after = matrix[:, :, 1] > matrix[:, :, 0]
+        # pairs i,j where matrix[i][j][1] > matrix[i, j, 2] i.e. i tied with j cheaper than i after j
+        tied_cheaper_after = matrix[:, :, 1] > matrix[:, :, 2]
+        after_not_cheapest = logical_or(before_cheaper_after, tied_cheaper_after)
+        # arcs of the graph: pairs (i, j) where cost of i after j is not the cheapest
+        # arcs should be added all at once, the impact on performances is clear
+        graph_of_elements.add_edges(list(column_stack(where(after_not_cheapest))))
 
-        if put_before > put_after or put_before > put_tied:
-            arcs.append((el_2, el_1))
-        # if y should not be placed after x, then arc from y to x
-        if put_after > put_before or put_after > put_tied:
-            arcs.append((el_1, el_2))
-
-        if put_before < put_after and put_before < put_tied:
-            robust_arcs.add((el_1, el_2))
-        if put_after < put_before and put_after < put_tied:
-            robust_arcs.add((el_2, el_1))
-
-    @staticmethod
-    def _fill_graph(cost_matrix: ndarray, el_1: int, el_2: int, arcs: List[Tuple[int, int]]):
-        """
-        Function to be called by a callback when the graph of elements must be computed.
-
-        :param cost_matrix: The 3D cost matrix.
-        :param el_1: The 1st element.
-        :param el_2: The 2nd element.
-        :param arcs: The list of arcs to be modified as a List of Tuple of int, int.
-        :return: None.
-        """
-        put_before: float = cost_matrix[0]
-        put_after: float = cost_matrix[1]
-        put_tied: float = cost_matrix[2]
-
-        # if x should not be placed after y, then arc from x to y
-        if put_before > put_after or put_before > put_tied:
-            arcs.append((el_2, el_1))
-        # if y should not be placed after x, then arc from y to x
-        if put_after > put_before or put_after > put_tied:
-            arcs.append((el_1, el_2))
+        return graph_of_elements
 
     @staticmethod
     def can_be_all_tied(id_elements_to_check: Set[int], cost_matrix: ndarray) -> bool:
